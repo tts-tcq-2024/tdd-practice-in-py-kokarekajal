d12426b55b58b5487dcda1378df06180079b16da
Kajal Kokare <162293149+kokarekajal@users.noreply.github.com>
2024-07-22 19:14:03 +0530

    testcae02_Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -1,26 +1,25 @@
 import unittest
-from StringCalculator import StringCalculator
+from your_module_name import StringCalculator  # Update with the actual module name
+
 class TestStringCalculator(unittest.TestCase):
+
+    def test_expectZeroForEmptyInput(self):
+        self.assertEqual(StringCalculator.add(""), 0)
+    
+    def test_expectZeroForSingleZero(self):
+        self.assertEqual(StringCalculator.add("0"), 0)
         
-        def test_expectZeroForEmptyInput(self):
-                self.assertEqual(add(""), 0)
-          
-        def test_expectZeroForSingleZero(self):
-                self.assertEqual(add("0"), 0)
-                
-        def test_expectSumForTwoNumberst(self):
-                self.assertEqual(add("1,2"), 3)
-                
-        def test_ignoreNumbersGreaterThan1000(self):
-                self.assertEqual(add("1,1001"), 1)
-                
-        def test_expectSumWithCustomDelimiter(self):
-                self.assertEqual(add("//;\n1;2"), 3)
-                
-        def test_expectSumWithNewlineDelimiter(self):
-                self.assertEqual(add("1\n2,3"),6);
+    def test_expectSumForTwoNumbers(self):
+        self.assertEqual(StringCalculator.add("1,2"), 3)
         
+    def test_ignoreNumbersGreaterThan1000(self):
+        self.assertEqual(StringCalculator.add("1,1001"), 1)
+        
+    def test_expectSumWithCustomDelimiter(self):
+        self.assertEqual(StringCalculator.add("//;\n1;2"), 3)
+        
+    def test_expectSumWithNewlineDelimiter(self):
+        self.assertEqual(StringCalculator.add("1\n2,3"), 6)
+
 if __name__ == '__main__':
-        unittest.main()
-
-
+    unittest.main()
