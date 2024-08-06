b5e098ad14dcda631779f193f758ba2b2c6f5f9a
Kajal Kokare <162293149+kokarekajal@users.noreply.github.com>
2024-08-06 15:25:56 +0530

    Update1 StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -1,24 +1,9 @@
 import unittest
-from StringCalculator import StringCalculator
+from StringCalculator import add
+
 class TestStringCalculator(unittest.TestCase):
-        
-        def test_expectZeroForEmptyInput(self):
-                self.assertEqual(add(""), 0)
-          
-        def test_expectZeroForSingleZero(self):
-                self.assertEqual(add("0"), 0)
-                
-        def test_expectSumForTwoNumberst(self):
-                self.assertEqual(add("1,2"), 3)
-                
-        def test_ignoreNumbersGreaterThan1000(self):
-                self.assertEqual(add("1,1001"), 1)
-                
-        def test_expectSumWithCustomDelimiter(self):
-                self.assertEqual(add("//;\n1;2"), 3)
-                
-        def test_expectSumWithNewlineDelimiter(self):
-                self.assertEqual(add("1\n2,3"),6);
-        
+    def test_empty_string(self):
+        self.assertEqual(add(""), 0)
+
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
