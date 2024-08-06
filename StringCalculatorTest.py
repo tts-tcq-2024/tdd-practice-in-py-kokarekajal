a7d8d984913c1295f55f0589326ae36f9f012168
Kajal Kokare <162293149+kokarekajal@users.noreply.github.com>
2024-08-06 15:29:15 +0530

    Update2 StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -5,5 +5,8 @@ class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
 
+    def test_single_number(self):
+        self.assertEqual(add("1"), 1)
+
 if __name__ == '__main__':
     unittest.main()
